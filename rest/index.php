677c23b8c0a66ec24648b8f6e9241c3177a99023
Jijo James <jijojames18@gmail.com>
2019-03-16 12:08:57 +0530

    Added rest end point for rest

---
+++ rest/index.php
@@ -0,0 +1,37 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$app->post('/v1/contact', function (Request $request, Response $response, array $args) {
+    $parsedBody = $request->getParsedBody();
+    $name = isset($parsedBody['name']) ? $parsedBody['name'] : 'Name';
+    $message = isset($parsedBody['message']) ? $parsedBody['message'] : 'Message';
+    sendEmail(escapeRequestData($name), escapeRequestData($message));
+    return $response;
+});
+
+$app->run();
+
+function escapeRequestData ($data) {
+    $data = stripslashes($data);
+    $data = htmlentities($data);
+    $data = strip_tags($data);
+    return $data;
+}
+
+function sendEmail ($name, $body) {
+    $to = "jijojames18@gmail.com";
+    $from = "contact@jijojames.com";
+    $subject = "Contact Form [jijojames.com] : " . $name;
+
+    $message = $body;
+
+    $header  = "From:" . $from . " \r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-type: text/html\r\n";
+
+    return mail ($to, $subject, $message, $header);
+}
