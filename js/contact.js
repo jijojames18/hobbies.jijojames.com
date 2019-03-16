086abea4d4d25c0fce7b9002db31c7935b953410
Jijo James <jijojames18@gmail.com>
2019-03-16 14:25:59 +0530

    recaptcha for about page

--- js/contact.js
+++ js/contact.js
@@ -4,7 +4,8 @@ class ContactForm extends React.Component {
       this.state = {
         name: '',
         email: '',
-        message: ''
+        message: '',
+        errorMessage: ''
       };
   
       this.handleInputChange = this.handleInputChange.bind(this);
@@ -12,23 +13,33 @@ class ContactForm extends React.Component {
     }
   
     handleSubmit() {
-        $.ajax({
-            method: "POST",
-            url: REST_ENDPOINT_CONTACT,
-            data: { name: this.state.name, message: this.state.message, email: this.state.email }
-        })
-        .done(function(msg) {
-        });
+        if (grecaptcha.getResponse())
+        {
+            $.ajax({
+                method: "POST",
+                url: REST_ENDPOINT_CONTACT,
+                data: { name: this.state.name, message: this.state.message, email: this.state.email }
+            })
+            .done(function(msg) {
+            });
+        }
+        else
+        {
+            this.setState({
+                ['errorMessage']: "I guess you are a robot!!!..."
+            });
+            event.preventDefault();
+        }
     }
 
     handleInputChange(event) {
-      const target = event.target;
-      const value = target.value;
-      const name = target.name;
-  
-      this.setState({
-        [name]: value
-      });
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+    
+        this.setState({
+            [name]: value
+        });
     }
   
     render() {
@@ -41,6 +52,11 @@ class ContactForm extends React.Component {
                 <div class="modal-body">
                     <div class="col-md-6 col-md-offset-3">
                         <form id="contact" action="" method="POST" onSubmit={this.handleSubmit}>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h4 class="form-error">{this.state.errorMessage}</h4>
+                                </div>
+                            </div>
                             <div class="row">
                                 <div class="col-md-12">
                                     <fieldset>
@@ -59,6 +75,8 @@ class ContactForm extends React.Component {
                                         <textarea name="message" rows="6" class="form-control" id="message" value={this.state.message}
                                         onChange={this.handleInputChange} placeholder="Your message..." required></textarea>
                                     </fieldset>
+                                </div>
+                                <div class="col-md-12 reCaptcha-container" id="reCaptcha-container">
                                 </div>
                                 <div class="col-md-12">
                                     <fieldset>
--- js/main.js
+++ js/main.js
@@ -1,53 +1,59 @@
 var REST_ENDPOINT_CONTACT = "https://jijojames.com/rest/index.php/v1/contact";
 
-jQuery(document).ready(function($) {
-  'use strict';
+function onCaptchaCallback () {
+    grecaptcha.render('reCaptcha-container', {
+        'sitekey' : '6LdzBpgUAAAAAAKB2fzfWeixUIb6eXC2h9KkNqRz'
+    });
+}
 
-  $(function() {
+jQuery(document).ready(function($) {
+    'use strict';
+
+    $(function() {
     var modBtn     = $('#modBtn'),
         modal      = $('#modal'),
         close      = modal.find('.close-btn img'),
         modContent = modal.find('.modal-content');
- 
+
     modBtn.on('click', function() {
-      modal.css('display', 'block');
-      modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+        modal.css('display', 'block');
+        modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
     });
 
     $(document).on('click', function(e) {
-      var target = $(e.target);
-      if (target.is(modal) || target.is(close)) 
-      {
-        modContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
-          modal.css('display', 'none');
-          next();
+        var target = $(e.target);
+        if (target.is(modal) || target.is(close)) 
+        {
+            modContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
+                modal.css('display', 'none');
+                next();
+            });
+        }
+    });
+
+    });
+
+    $('a.scrollTo').on('click', function() {
+        var scrollTo = $(this).attr('data-scrollTo');
+        $( "a.scrollTo" ).each(function() {
+            if (scrollTo == $(this).attr('data-scrollTo'))
+            {
+                $(this).addClass('active');
+            }
+            else
+            {
+                $(this).removeClass('active');
+            }
         });
-      }
-    });
-    
-  });
 
-  $('a.scrollTo').on('click', function() {
-    var scrollTo = $(this).attr('data-scrollTo');
-    $( "a.scrollTo" ).each(function() {
-      if (scrollTo == $(this).attr('data-scrollTo'))
-      {
-        $(this).addClass('active');
-      }
-      else
-      {
-        $(this).removeClass('active');
-      }
+        $('body, html').animate({
+            "scrollTop": $('#'+scrollTo).offset().top
+        }, 1000);
+        return false;
     });
 
-    $('body, html').animate({
-      "scrollTop": $('#'+scrollTo).offset().top
-    }, 1000);
-    return false;
-  });
-
-  $(".menu-icon").click(function() {
-    $(this).toggleClass("active");
-    $(".overlay-menu").toggleClass("open");
-  });
+    $(".menu-icon").click(function() {
+        $(this).toggleClass("active");
+        $(".overlay-menu").toggleClass("open");
+    });
 });
