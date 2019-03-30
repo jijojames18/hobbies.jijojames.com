4227c684158af97b7ca99c496e41cf21ad4d74d4
Jijo James <jijojames18@gmail.com>
2019-03-30 12:31:45 +0530

    Added load more for gallery

--- js/gallery.js
+++ js/gallery.js
@@ -2,45 +2,79 @@ class Gallery extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            'images': []
+            'shownImages': [],
+            'count': 0,
+            'allImages': []
         };
+        this.loadMore = this.loadMore.bind(this);
+        this.imagesShownInEachClick = 12;
     }
 
     componentWillMount() {
-        var that = this;
+        let that = this;
+
         fetch(REST_ENDPOINT_GALLERY).then(function (response) {
             return response.json();
         }).then(function (result) {
-            var data = {
-                'images': result
+            let shownImages = result.filter(function(image, index) { return index < that.imagesShownInEachClick });
+            let data = {
+                'allImages': result,
+                'count': result.length,
+                'shownImages': shownImages
             };
             that.setState(data);
         });
     }
 
+    loadMore() {
+        let shownImages = this.state.shownImages;
+        let shownImagesLength = shownImages.length;
+        let newImages = this.state.allImages.filter(function(image, index) { 
+            return index < shownImagesLength + this.imagesShownInEachClick && index >= shownImagesLength 
+        }.bind(this));
+        shownImages = shownImages.concat(newImages);
+
+        this.setState({
+            'shownImages': shownImages
+        });
+    }
+
     render() {
         return (
-            <div>{this.state.images.map((image, i) =>
-                <div class="col-md-4 col-sm-6">
-                    <div class="portfolio-item">
-                        <a
-                            href={image.file_url}
-                            data-lightbox="image-1"><div class="thumb">
-                                <div class="hover-effect">
-                                    <div class="hover-content">
-                                        <h1>{image.place}</h1>
-                                        <p>{image.date}</p>
-                                    </div>
-                                </div>
-                                <div class="image">
-                                    <img
-                                        src={image.file_url}
-                                        width="360" height="360" />
-                                </div>
-                            </div></a>
-                    </div>
+            <div>
+                <div class="row">
+                    {this.state.shownImages.map((image, i) =>
+                        <div class="col-md-4 col-sm-6">
+                            <div class="portfolio-item">
+                                <a
+                                    href={image.file_url}
+                                    data-lightbox="image-1"><div class="thumb">
+                                        <div class="hover-effect">
+                                            <div class="hover-content">
+                                                <h1>{image.place}</h1>
+                                                <p>{image.date}</p>
+                                            </div>
+                                        </div>
+                                        <div class="image">
+                                            <img
+                                                src={image.file_url}
+                                                width="360" height="360" />
+                                        </div>
+                                    </div></a>
+                            </div>
+                        </div>
+                    )}
                 </div>
-            )}</div>
+                {this.state.shownImages.length < this.state.allImages.length &&
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="load-more-button">
+                                <a href="#" onClick={this.loadMore}>Load More</a>
+                            </div>
+                        </div>
+                    </div>
+                }
+            </div>
         );
     }
 }
