e93ea5d691d5d5ef983c3430684695c36806f184
Jijo James <jijojames18@gmail.com>
2019-03-30 21:51:24 +0530

    Build script

--- rest/classes/blog.php
+++ rest/classes/blog.php
@@ -7,7 +7,7 @@ class Blog {
     }
 
     private function createDatabaseConnection() {
-        $this->db = new PDO("mysql:host=localhost;dbname=". DB_NAME, DB_USER, DB_PASSWORD);
+        $this->db = new PDO("mysql:host=localhost;dbname=". DB_NAME. ";charset=UTF8", DB_USER, DB_PASSWORD);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     }
