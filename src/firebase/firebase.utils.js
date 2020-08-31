d56856ff2b423b2e0af8943837946f8765262be5
Jijo James <jijojames18@gmail.com>
2020-08-31 19:11:42 +0530

    Get function for fetching from firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,22 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-firebase.initializeApp(process.env.REACT_APP_FIREBASE_CONFIG);
+firebase.initializeApp(JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG));
+
+export const firestore = firebase.firestore();
+
+export const fetchDocumentsFromStore = async (collectionName, from, limit) => {
+  const collection = firestore.collection(collectionName);
+
+  const query = collection
+    .where(firebase.firestore.FieldPath.documentId(), ">", from)
+    .where(firebase.firestore.FieldPath.documentId(), "<=", limit);
+
+  const snapshot = await query.get();
+  return snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+};
 
 export default firebase;
