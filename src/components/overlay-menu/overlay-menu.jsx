b996ebf1a9e0600e05b994c3960e226a415a4ee8
Jijo James <jijojames18@gmail.com>
2020-08-31 09:07:03 +0530

    Overlay Menu

---
+++ src/components/overlay-menu/overlay-menu.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+import {
+  OverlayMenuContainer,
+  MainMenu,
+  Ul,
+  Li,
+  Link,
+} from "./overlay-menu.styles";
+
+const OverlayMenu = ({ isMenuOpen }) => {
+  const menuContainerRef = React.createRef();
+
+  useEffect(() => {
+    isMenuOpen
+      ? menuContainerRef.current.classList.add("open")
+      : menuContainerRef.current.classList.remove("open");
+  }, [isMenuOpen, menuContainerRef]);
+
+  return (
+    <OverlayMenuContainer ref={menuContainerRef}>
+      <Container>
+        <Row>
+          <MainMenu>
+            <Ul>
+              <Li>
+                <Link href="/">Home</Link>
+              </Li>
+              <Li>
+                <Link href="/blogs">Blog</Link>
+              </Li>
+              <Li>
+                <Link href="/videos">Videos</Link>
+              </Li>
+            </Ul>
+          </MainMenu>
+        </Row>
+      </Container>
+    </OverlayMenuContainer>
+  );
+};
+
+export default OverlayMenu;
---
+++ src/components/overlay-menu/overlay-menu.styles.jsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const OverlayMenuContainer = styled.section`
+  position: fixed;
+  z-index: 0;
+  left: 0;
+  top: 80px;
+  height: 100%;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+  width: 100%;
+  text-align: center;
+  overflow: scroll;
+  transform: translateY(-100%);
+  transition: all 0.5s;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  &.open {
+    background: rgba(0, 0, 0, 0.95);
+    color: #fff;
+    opacity: 1;
+    transform: translateY(0%);
+    z-index: 100;
+
+    div {
+      transition: all 0.7s;
+      transition-delay: 0.7s;
+      opacity: 1;
+      transform: translateY(0%);
+    }
+  }
+`;
+
+export const MainMenu = styled.div`
+  transform: translateY(50%);
+  opacity: 0;
+  transition: all 0.3s;
+  transition-delay: 0s;
+
+  &:nth-child(2) {
+    transition-delay: 0s;
+  }
+`;
+
+export const Ul = styled.ul`
+  list-style: none;
+  margin-top: 80px;
+  padding: 0px;
+`;
+
+export const Li = styled.li`
+  padding: 15px 0px;
+`;
+
+export const Link = styled.a`
+  font-size: 24px;
+  font-weight: 300;
+  color: #fff;
+  text-decoration: none;
+  transition: all 0.5s;
+
+  &:hover {
+    color: rgba(250, 250, 250, 0.5);
+  }
+`;
