e0b40248721d4be584fed5e9b8ccf40830e97ac9
Jijo James <jijojames18@gmail.com>
2020-09-02 17:00:47 +0530

    Added common styles

---
+++ src/styles/common.styles.jsx
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+export const ContainerPaddingTop = "100px";
+
+export const ContentColor = "#232323";
+
+export const OverlayBackgroundColor = "#0d0d0d";
+
+export const ContainerComponent = styled.div`
+  padding-top: ${ContainerPaddingTop};
+  background-color: ${ContentColor};
+`;
+
+export const GalleryContainerComponent = styled.div`
+  background-color: ${ContentColor};
+`;
+
+export const ButtonStyles = css`
+  border-radius: 0px;
+  font-size: 14px;
+  text-transform: uppercase;
+  font-weight: 600;
+  background-color: transparent;
+  letter-spacing: 2px;
+  border-bottom: 4px solid #fff;
+  padding-bottom: 4px;
+  text-decoration: none;
+  transition: all 0.5s;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
