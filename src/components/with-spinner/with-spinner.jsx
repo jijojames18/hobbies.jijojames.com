ec6d0eaad680d43334b3d02925f32fe64e0e5f85
Jijo James <jijojames18@gmail.com>
2020-09-03 15:22:32 +0530

    Removed with spinner

--- src/components/with-spinner/with-spinner.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Spinner from "../spinner/spinner";
-
-const WithSpinner = (WrappedComponent) => ({ isLoading, ...props }) => {
-  return isLoading ? <Spinner /> : <WrappedComponent {...props} />;
-};
-
-export default WithSpinner;
