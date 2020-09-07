a83fcd0fc26d537d0961afbc9f5c498665f3b4a7
Jijo James <jijojames18@gmail.com>
2020-09-07 21:51:06 +0530

    New component for project item

--- src/components/project-card-item/project-card-item.jsx
+++ src/components/project-card-item/project-card-item.jsx
@@ -1,8 +1,65 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
-const ProjectCardItem = () => {
-  return <Col sm={12} md={6} lg={4}></Col>;
+import {
+  CardListGroup,
+  CardListGroupItem,
+  CardBodyMainContent,
+  CardBodyMainTitle,
+  CardBodyMainText,
+  CardLink,
+} from "./project-card-item.styles";
+
+const ProjectCardItem = ({
+  item: {
+    title,
+    image,
+    desc,
+    technology,
+    github_link: githubLink,
+    site_link: siteLink,
+  },
+}) => {
+  const listVariants = [
+    "primary",
+    "secondary",
+    "succes",
+    "danger",
+    "warning",
+    "info",
+    "link",
+    "dark",
+  ];
+  return (
+    <Col sm={12} md={6} lg={4}>
+      <Card bg="secondary">
+        <Card.Body>
+          <Card.Img variant="top" src={image}></Card.Img>
+          <CardListGroup horizontal>
+            {technology.map((item, i) => (
+              <CardListGroupItem variant={listVariants[i]} key={i}>
+                {item}
+              </CardListGroupItem>
+            ))}
+          </CardListGroup>
+        </Card.Body>
+        <CardBodyMainContent>
+          <CardBodyMainTitle>{title}</CardBodyMainTitle>
+          <CardBodyMainText>{desc}</CardBodyMainText>
+        </CardBodyMainContent>
+
+        <Card.Footer className="text-muted">
+          <CardLink href={siteLink} rel="noopener noreferrer" target="_blank">
+            Website
+          </CardLink>
+          <CardLink href={githubLink} rel="noopener noreferrer" target="_blank">
+            Github
+          </CardLink>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
 };
 
 export default ProjectCardItem;
--- src/components/project-card-item/project-card-item.styles.jsx
+++ src/components/project-card-item/project-card-item.styles.jsx
@@ -1,1 +1,32 @@
 import styled from "styled-components";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
+
+export const CardListGroup = styled(ListGroup)`
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+`;
+
+export const CardListGroupItem = styled(ListGroupItem)`
+  font-size: 14px;
+  margin: 8px 0;
+`;
+
+export const CardBodyMainContent = styled(Card.Body)`
+  margin-top: 0px;
+  padding-top: 0px;
+`;
+
+export const CardBodyMainTitle = styled(Card.Title)`
+  color: #fff;
+  font-weight: bold;
+`;
+
+export const CardBodyMainText = styled(Card.Text)`
+  text-align: justify;
+`;
+
+export const CardLink = styled(Card.Link)`
+  font-size: 16px;
+`;
