7d1478e7194818fbdd7433a9319126d80a512fe6
Jijo James <jijojames18@gmail.com>
2020-09-11 19:06:24 +0530

    About page

---
+++ src/views/about/about.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { ContainerComponent } from "../../styles/common.styles";
+
+const AboutPage = () => {
+  return <ContainerComponent></ContainerComponent>;
+};
+
+export default AboutPage;
