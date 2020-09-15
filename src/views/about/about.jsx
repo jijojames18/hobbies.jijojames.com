cc1c4aec00f45c1d17871ef674a991c7d264f504
Jijo James <jijojames18@gmail.com>
2020-09-15 20:35:21 +0530

    Added My Skills component to About Page

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { AboutContainerComponent } from "../../styles/common.styles";
 import CareerTimeline from "../../components/career-timeline/career-timeline";
 import AboutMe from "../../components/about-me/about-me";
+import MySkills from "../../components/my-skills/my-skills";
 
 const AboutPage = () => {
   return (
     <AboutContainerComponent>
       <AboutMe />
       <CareerTimeline />
+      <MySkills />
     </AboutContainerComponent>
   );
 };
