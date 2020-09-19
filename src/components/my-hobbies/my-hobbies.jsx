58a71c7bb821ba186dbd942d56e3e864a0699782
Jijo James <jijojames18@gmail.com>
2020-09-19 18:42:17 +0530

    My Hobbies component

--- src/components/my-hobbies/my-hobbies.jsx
+++ src/components/my-hobbies/my-hobbies.jsx
@@ -1,46 +1,58 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import cameraImage from "../../assets/camera-icon.svg";
-import blogImage from "../../assets/blog-icon.svg";
-import readingImage from "../../assets/reading-icon.svg";
-import footballImage from "../../assets/football-icon.svg";
-import vlogImage from "../../assets/vlog-icon.svg";
+import Row from "react-bootstrap/Row";
 
-import { MyHobbiesContainer, CardContainer } from "./my-hobbies.styles";
+import FlipCard from "../flip-card/flip-card";
+
+import {
+  MyHobbiesContainer,
+  MyHobbiesHeadingCol,
+  MyHobbiesHeading,
+  FlipCardPhotographyImg,
+  FlipCardBlogImg,
+  FlipCardReadingImg,
+  FlipCardVlogImg,
+  MyHobbiesContentCol,
+  FlipCardFootballImg,
+} from "./my-hobbies.styles";
+
+const Hobbies = [
+  {
+    img: FlipCardPhotographyImg,
+    title: "Photography",
+  },
+  {
+    img: FlipCardBlogImg,
+    title: "Blogging",
+  },
+  {
+    img: FlipCardVlogImg,
+    title: "Vlogging",
+  },
+  {
+    img: FlipCardReadingImg,
+    title: "Reading",
+  },
+  {
+    img: FlipCardFootballImg,
+    title: "Football",
+  },
+];
 
 const MyHobbies = () => {
   return (
     <MyHobbiesContainer>
-      <CardContainer>
-        <Card.Img variant="top" src={cameraImage} />
-        <Card.Body>
-          <Card.Title>Photography</Card.Title>
-        </Card.Body>
-      </CardContainer>
-      <CardContainer>
-        <Card.Img variant="top" src={blogImage} />
-        <Card.Body>
-          <Card.Title>Blogging</Card.Title>
-        </Card.Body>
-      </CardContainer>
-      <CardContainer>
-        <Card.Img variant="top" src={readingImage} />
-        <Card.Body>
-          <Card.Title>Reading</Card.Title>
-        </Card.Body>
-      </CardContainer>
-      <CardContainer>
-        <Card.Img variant="top" src={footballImage} />
-        <Card.Body>
-          <Card.Title>Football</Card.Title>
-        </Card.Body>
-      </CardContainer>
-      <CardContainer>
-        <Card.Img variant="top" src={vlogImage} />
-        <Card.Body>
-          <Card.Title>Vlogging</Card.Title>
-        </Card.Body>
-      </CardContainer>
+      <Row>
+        <MyHobbiesHeadingCol>
+          <MyHobbiesHeading>My Hobbies</MyHobbiesHeading>
+        </MyHobbiesHeadingCol>
+      </Row>
+      <Row>
+        <MyHobbiesContentCol>
+          {Hobbies.map((hobby, i) => (
+            <FlipCard key={i} item={hobby} />
+          ))}
+        </MyHobbiesContentCol>
+      </Row>
     </MyHobbiesContainer>
   );
 };
--- src/components/my-hobbies/my-hobbies.styles.jsx
+++ src/components/my-hobbies/my-hobbies.styles.jsx
@@ -1,17 +1,65 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import Col from "react-bootstrap/Col";
 
-import Card from "react-bootstrap/Card";
+import cameraImage from "../../assets/camera-icon.svg";
+import blogImage from "../../assets/blog-icon.svg";
+import readingImage from "../../assets/reading-icon.svg";
+import footballImage from "../../assets/football-icon.svg";
+import vlogImage from "../../assets/vlog-icon.svg";
 
 export const MyHobbiesContainer = styled.div`
   display: flex;
-  justify-content: space-evenly;
-  margin-top: 14px;
-  margin-bottom: 14px;
+  justify-content: center;
+  flex-direction: column;
+  margin: 14px 0 24px 0;
+  font-family: "Roboto";
 }
 `;
 
-export const CardContainer = styled(Card)`
-  background-color: blue;
-  align-items: center;
-  border-radius: 50px;
+export const MyHobbiesHeadingCol = styled(Col)`
+  text-align: center;
+  padding: 24px 0;
+  width: 100%;
+`;
+
+export const MyHobbiesHeading = styled.h3``;
+
+export const MyHobbiesContentCol = styled(Col)`
+  display: flex;
+  justify-content: center;
+  @media screen and (max-width: 600px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const FlipCardImg = css`
+  width: 64px;
+  height: 64px;
+  background-size: cover;
+`;
+
+export const FlipCardPhotographyImg = styled.div`
+  background-image: url(${cameraImage});
+  ${FlipCardImg}
+`;
+
+export const FlipCardVlogImg = styled.div`
+  background-image: url(${vlogImage});
+  ${FlipCardImg}
+`;
+
+export const FlipCardReadingImg = styled.div`
+  background-image: url(${readingImage});
+  ${FlipCardImg}
+`;
+
+export const FlipCardBlogImg = styled.div`
+  background-image: url(${blogImage});
+  ${FlipCardImg}
+`;
+
+export const FlipCardFootballImg = styled.div`
+  background-image: url(${footballImage});
+  ${FlipCardImg}
 `;
