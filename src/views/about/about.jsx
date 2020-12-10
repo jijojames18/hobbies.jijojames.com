e291f56cd76ac034a3e1ab018c508d8dd446d40b
Jijo James <jijojames18@gmail.com>
2020-12-10 21:15:45 +0530

    Disabling not needed components

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { AboutContainerComponent } from "../../styles/common.styles";
 import CareerTimeline from "../../components/career-timeline/career-timeline";
 import AboutMe from "../../components/about-me/about-me";
-import MySkills from "../../components/my-skills/my-skills";
-import MyHobbies from "../../components/my-hobbies/my-hobbies";
+//import MySkills from "../../components/my-skills/my-skills";
+//import MyHobbies from "../../components/my-hobbies/my-hobbies";
 
 const AboutPage = () => {
   return (
     <AboutContainerComponent>
       <AboutMe />
-      <CareerTimeline />
-      <MySkills />
-      <MyHobbies />
+      <CareerTimeline />{
+        //<MySkills />
+        //<MyHobbies />
+      }
     </AboutContainerComponent>
   );
 };
